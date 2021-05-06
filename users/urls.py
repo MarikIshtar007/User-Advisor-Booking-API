00b6c2866d60d83baebcadbca044309a2d115fe8
Haany Ali <46514188+MarikIshtar007@users.noreply.github.com>
2021-05-06 21:34:22 +0530

    Added requirements.txt

--- users/urls.py
+++ users/urls.py
@@ -7,8 +7,7 @@ from advisor import views as adv_views
 urlpatterns = [
     path('user/login/', views.UserLoginApiView.as_view()),
     path('user/register/', views.UserRegisterApiView.as_view()),
-    path('user/<user_id>/advisor/', adv_views.ShowAdvisors.as_view(),name='show-advisors'),
-    path('user/<int:user_id>/advisor/<int:advisor_id>/', adv_views.BookAdvisors.as_view(), name='book-advisors'),
+    path('user/<user_id>/advisor/', adv_views.ShowAdvisors.as_view()),
+    path('user/<int:user_id>/advisor/<int:advisor_id>/', adv_views.BookAdvisors.as_view()),
     path('user/<int:user_id>/advisor/booking/',adv_views.ShowAppointments.as_view())
-
 ]
